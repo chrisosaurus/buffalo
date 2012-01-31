2293780ba13a26f50067eb36b8d09bceba358e53
Chris Hall <followingthepath@gmail.com>
2012-01-31 18:40:22 +1300

    made binaries appear in bin

--- make.sh
+++ make.sh
@@ -1,3 +1,7 @@
-gcc editor.c codes.c -o buffalo
-gcc cbtree.c cbtree_test.c -o cbtree_test
-gcc forking.c -o forking
+if [ ! -e bin ] ; then
+  mkdir bin
+fi
+
+gcc editor.c codes.c -o bin/buffalo
+gcc cbtree.c cbtree_test.c -o bin/cbtree_test
+gcc forking.c -o bin/forking
