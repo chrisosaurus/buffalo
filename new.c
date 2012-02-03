c240ab690ecc7e40d6e66366b3780165f1d53805
Chris Hall <chrish@catalyst.net.nz>
2012-02-03 16:53:52 +1300

    fixed silly errors, added some more boiler plate

--- new.c
+++ new.c
@@ -1,5 +1,7 @@
 #include <stdbool.h> /* bool, true and false */
 #include "codes.h" /* because ncurses sucks more */
+#include <stdlib.h> /* realloc, malloc, calloc */
+#include <string.h> /* memmove */
 
 #define LINESIZE 80
 
@@ -34,12 +36,13 @@ typedef struct { /* key binding */
 static Line *start=0, *end=0; /* first and last lines */
 static Line *screen = 0; /* first line on screen */
 static Filepos cur = { 0, 0 }; /* current position in file */
+static tstate orig = 0; /* original terminal state */
 
 /** Internal functions **/
 static Filepos i_insert(const char *c, Filepos pos); /* insert c at pos and return new filepos after the inserted char */
 static int i_utf8len(const unsigned char *c); /* return number of bytes of utf char c */
-static void i_setup(); /* setup the terminal for editing */
-static void i_tidyup(); /* clean up and return the terminal to it's original state */
+static void i_setup(void); /* setup the terminal for editing */
+static void i_tidyup(void); /* clean up and return the terminal to it's original state */
 
 /** Movement functions **/
 static Filepos m_bof(Arg arg); /* move to beginning of file */
@@ -58,11 +61,11 @@ i_insert(const char *c, Filepos pos){
 		if( c[i] == '\n' ){
 			/* FIXME handle newline special case */
 		} else {
-			if( l->len >= (l->m * LINESIZE) ) /* FIXME need to take \0 at end of each string */
-				l->c = (char*)realloc(l->c, LINESIZE(++(l->m)));
+			if( l->l >= (l->m * LINESIZE) ) /* FIXME need to take \0 at end of each string */
+				l->c = (char*)realloc(l->c, LINESIZE*(++(l->m)));
 			memmove(l->c+pos.o+1, l->c+pos.o, l->l - pos.o); /* FIXME check black magic */
-			l->c+pos.o = c[i];
-			l->d = TRUE;
+			*(l->c+pos.o) = c[i];
+			l->d = true;
 			/* FIXME need a \0 at the end of each string */
 		}
 	}
@@ -78,11 +81,26 @@ i_utf8len(const unsigned char *c){
 	return 1;
 }
 
+void
+i_setup(void){
+    /* TODO write state to orig, modify local copy and set */
+    orig = t_getstate();
+    tstate new = orig;
+
+}
+
+void
+i_tidyup(void){
+    /* TODO restore tstate and stuff */
+}
+
 int /* the magic main function */
 main(int argc, char **argv){
 	/* TODO setup */
+    i_setup();
 	while( 1 ){
 		/* TODO main loop, check input, see what happend next */
 	}
 	/* TODO tidy up */
+    i_tidyup();
 }
