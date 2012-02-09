b129d1e76c5687de34a1ede3aafb460f9b70fa1c
Chris Hall <chrish@catalyst.net.nz>
2012-02-09 16:04:54 +1300

    added trythis, proving puts and printf behave

---
+++ trythis.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <stdlib.h>
+
+int main(){
+    struct termios term, bkp;
+
+    if( tcgetattr(1, &term) ){
+        perror("failed to get termios\n");
+        exit(1);
+    }   
+    bkp = term;
+    term.c_lflag = 0; // set non-canonical
+    term.c_cc[VTIME] = 0; // set timeout
+    term.c_cc[VMIN] = 1; // set read to return after it has one char available
+    //tcflush(1, TCIFLUSH);
+    if( tcsetattr(1, TCSANOW, &term) ){
+        perror("Failed to set termios\n");
+        exit(1);
+    }  
+
+    puts("[2J");
+    puts("[0m");
+    puts("[0;0H");
+    printf("hello world!!!!\n");
+    puts("[32m");
+    puts("GREEEEEEENNN");
+    printf("%c[33m%s", 0x1b, "YELLOW");
+}
